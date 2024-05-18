9d05873443f2297104d4d36e55a27b9c812bb558
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2024-05-18 10:46:38 -0700

    epd

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # xenagogy
 
 Android SVG editor optimized for e-ink note-taking tablets, such as the Onyx Boox series.
+
+## Resources
+
+* <https://www.hanshq.net/command-line-android.html>.
+* `adb shell logcat MainActivity:V *:S`
+* `adb shell logcat *:E`
--- src/com/gilgamesh/xenagogy/R.java
+++ src/com/gilgamesh/xenagogy/R.java
@@ -10,7 +10,28 @@ package com.gilgamesh.xenagogy;
 public final class R {
     public static final class attr {
     }
+    public static final class id {
+        public static final int activity_main=0x7f030008;
+        public static final int button_enter_A2_mode=0x7f03000a;
+        public static final int button_enter_du_mode=0x7f03000b;
+        public static final int button_enter_fast_mode=0x7f030002;
+        public static final int button_enter_normal_mode=0x7f030009;
+        public static final int button_enter_x_mode=0x7f030007;
+        public static final int button_partial_update=0x7f030000;
+        public static final int button_quit_fast_mode=0x7f030003;
+        public static final int button_regal_partial=0x7f030001;
+        public static final int button_screen_refresh=0x7f030004;
+        public static final int fast_mode_layout=0x7f03000d;
+        public static final int layout_function=0x7f03000c;
+        public static final int my_text=0x7f03000e;
+        public static final int surfaceview=0x7f030006;
+        public static final int textview=0x7f030005;
+    }
+    public static final class layout {
+        public static final int activity_epd_demo=0x7f020000;
+        public static final int activity_main=0x7f020001;
+    }
     public static final class string {
-        public static final int hello_world=0x7f020000;
+        public static final int hello_world=0x7f040000;
     }
 }
