7fc80396576e61e7d7f7733173d1ba86229d35e2
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2024-05-25 09:38:32 -0700

    pan, erase

--- src/com/gilgamesh/xena/Xena.java
+++ src/com/gilgamesh/xena/Xena.java
@@ -1,10 +1,5 @@
 package com.gilgamesh.xena;
 
-import java.util.ArrayList;
-
-import android.graphics.Path;
-
 public class Xena {
 	public static final String TAG = "XENA";
-	public static ArrayList<Path> drawPaths = new ArrayList<Path>();
 }
