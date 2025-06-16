ede8839358953da3f837b82393ce15c5d7896696
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2025-06-16 02:35:36 -0600

    remove scrolling file picker

--- src/com/gilgamesh/xena/filesystem/FilePickerActivity.java
+++ src/com/gilgamesh/xena/filesystem/FilePickerActivity.java
@@ -1,7 +1,6 @@
 package com.gilgamesh.xena.filesystem;
 
 import com.gilgamesh.xena.scribble.ScribbleActivity;
-
 import com.gilgamesh.xena.R;
 import com.gilgamesh.xena.XenaApplication;
 
@@ -9,40 +8,91 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 
 public class FilePickerActivity extends Activity
 		implements View.OnClickListener {
 	private static final String SHARED_PREFERENCES_EDIT_TEXT_CACHE = "SHARED_PREFERENCES_EDIT_TEXT_CACHE";
+	private static final Point MIN_PANE_SIZE_DP = new Point(256, 48);
+	private static final LayoutParams LISTING_LAYOUT_ROW = new LayoutParams(
+			LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+	private static final int MARGIN_SIZE_DP = 4;
+
+	private Point MIN_PANE_SIZE_PX;
+	private int MARGIN_SIZE_PX;
+	private int PANES_PER_PAGE;
+	private Point PANE_SIZE;
+	private Point GRID_DIMENSIONS;
+	private LayoutParams LISTING_LAYOUT_PANE = null;
 
 	private EditText editText;
 	private LinearLayout layoutListing;
 	private SharedPreferences sharedPreferences;
+
+	private FilePickerTouchManager touchManager;
+
+	int page = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_file_picker);
 
+		this.editText = findViewById(R.id.activity_file_picker_edit_text);
+		this.layoutListing = findViewById(R.id.activity_file_picker_layout_listing);
+
+		// Finish init.
+		this.MIN_PANE_SIZE_PX = new Point((int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP,
+				FilePickerActivity.MIN_PANE_SIZE_DP.x,
+				this.getResources().getDisplayMetrics()),
+				(int) TypedValue.applyDimension(
+						TypedValue.COMPLEX_UNIT_DIP,
+						FilePickerActivity.MIN_PANE_SIZE_DP.y,
+						this.getResources().getDisplayMetrics()));
+		this.MARGIN_SIZE_PX = (int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP,
+				FilePickerActivity.MARGIN_SIZE_DP,
+				this.getResources().getDisplayMetrics());
+
+		this.layoutListing.getViewTreeObserver().addOnGlobalLayoutListener(
+				new ViewTreeObserver.OnGlobalLayoutListener() {
+					@Override
+					public void onGlobalLayout() {
+						layoutListing.getViewTreeObserver()
+								.removeOnGlobalLayoutListener(this);
+						onLayoutListingViewReady();
+					}
+				});
+
+		this.touchManager = new FilePickerTouchManager(this);
+		this.layoutListing.setOnTouchListener(this.touchManager);
+
+		// TODO: Deprecated.
 		this.sharedPreferences = PreferenceManager
 				.getDefaultSharedPreferences(this);
-
-		this.editText = findViewById(R.id.activity_file_picker_edit_text);
 		this.editText
 				.setText(this.sharedPreferences
 						.getString(FilePickerActivity.SHARED_PREFERENCES_EDIT_TEXT_CACHE,
@@ -60,12 +110,37 @@ public class FilePickerActivity extends Activity
 
 			@Override
 			public void afterTextChanged(Editable s) {
+				page = 0;
 				updateListing();
 			}
 		});
+	}
 
-		this.layoutListing = findViewById(R.id.activity_file_picker_layout_listing);
-		this.updateListing();
+	private void onLayoutListingViewReady() {
+		// Calculate PANE_SIZE.
+		this.GRID_DIMENSIONS = new Point(
+				this.layoutListing.getWidth()
+						/ (this.MIN_PANE_SIZE_PX.x + this.MARGIN_SIZE_PX * 2),
+				this.layoutListing.getHeight()
+						/ (this.MIN_PANE_SIZE_PX.y + this.MARGIN_SIZE_PX * 2));
+		this.PANES_PER_PAGE = this.GRID_DIMENSIONS.x * this.GRID_DIMENSIONS.y;
+		this.PANE_SIZE = new Point(
+				this.layoutListing.getWidth() / this.GRID_DIMENSIONS.x
+						- this.MARGIN_SIZE_PX * 2,
+				this.layoutListing.getHeight() / this.GRID_DIMENSIONS.y
+						- this.MARGIN_SIZE_PX * 2);
+		Log.v(XenaApplication.TAG, "FilePickerActivity::onLayoutListingViewReady: "
+				+ this.layoutListing.getWidth() + ", "
+				+ this.layoutListing.getHeight() + " | "
+				+ MIN_PANE_SIZE_PX + " | "
+				+ this.GRID_DIMENSIONS + " | " + this.PANE_SIZE);
+		this.LISTING_LAYOUT_PANE = new LayoutParams(this.PANE_SIZE.x,
+				this.PANE_SIZE.y);
+		this.LISTING_LAYOUT_PANE.setMargins(
+				this.MARGIN_SIZE_PX, this.MARGIN_SIZE_PX,
+				this.MARGIN_SIZE_PX, this.MARGIN_SIZE_PX);
+
+		updateListing();
 	}
 
 	@Override
@@ -73,17 +148,22 @@ public class FilePickerActivity extends Activity
 		String path = this.editText.getText().toString();
 
 		switch (v.getId()) {
-			case R.id.activity_file_picker_button:
+			case R.id.activity_file_picker_button_date:
+				this.editText.setText(this.editText.getText()
+						+ new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+				this.maybeStartScribbleActivity(true);
+				break;
+			case R.id.activity_file_picker_button_go:
 				this.maybeStartScribbleActivity(true);
 				break;
 			default:
 				TextView textView = (TextView) v;
 				String text = textView.getText().toString();
 				Log.v(XenaApplication.TAG,
-						"FilePickerActivity::onClick: Text: " + text);
+						"FilePickerActivity::onClick: Text: " + text + ".");
 
 				path = path.substring(0, path.lastIndexOf('/'));
-				if (text == "../") {
+				if (text.equals("../")) {
 					this.editText.setText(
 							path.substring(0, path.lastIndexOf('/')) + "/");
 				} else {
@@ -91,14 +171,15 @@ public class FilePickerActivity extends Activity
 				}
 				this.editText.setSelection(editText.getText().length());
 				this.maybeStartScribbleActivity(false);
-
 				break;
 		}
 	}
 
 	@Override
 	protected void onResume() {
-		this.updateListing();
+		if (this.LISTING_LAYOUT_PANE != null) {
+			this.updateListing();
+		}
 		super.onResume();
 	}
 
@@ -142,45 +223,66 @@ public class FilePickerActivity extends Activity
 		}
 	}
 
-	private void updateListing() {
+	void updateListing() {
 		this.layoutListing.removeAllViews();
 
 		String path = this.editText.getText().toString();
 		path = path.substring(0, path.lastIndexOf('/'));
 		Log.v(XenaApplication.TAG, "FilePickerActivity::updateListing: " + path);
 
-		this.addToListing("../", true);
-
 		File[] files = new File(path).listFiles();
 		if (files == null) {
-			return;
+			files = new File[0];
 		}
-		ArrayList<File> filesList = new ArrayList<File>();
-		for (File file : files) {
-			filesList.add(file);
-		}
+		ArrayList<File> filesList = new ArrayList<File>(Arrays.asList(files));
+		filesList.add(new File(".."));
 		Collections.sort(filesList, new Comparator<File>() {
 			@Override
 			public int compare(File a, File b) {
-				return a.getName().compareTo(b.getName());
+				if (a.isDirectory() && !b.isDirectory()) {
+					return -1;
+				}
+				if (!a.isDirectory() && b.isDirectory()) {
+					return 1;
+				}
+				return a.getName().toLowerCase().compareTo(b.getName().toLowerCase());
 			}
 		});
 
-		for (File file : filesList) {
-			this.addToListing(file.getName() + (file.isDirectory() ? "/" : ""),
-					file.isDirectory());
+		// Clamp page.
+		if (this.page < 0) {
+			this.page = 0;
 		}
-	}
+		if (this.page * this.PANES_PER_PAGE >= filesList.size()) {
+			this.page = filesList.size() / this.PANES_PER_PAGE;
+		}
 
-	private void addToListing(String element, boolean underline) {
-		TextView textView = new TextView(
-				new ContextThemeWrapper(this, R.style.directory_listing));
-		if (underline) {
-			textView
-					.setPaintFlags(
+		for (int i = 0; i < this.GRID_DIMENSIONS.y; i++) {
+			LinearLayout row = new LinearLayout(this);
+			for (int j = 0; j < this.GRID_DIMENSIONS.x; j++) {
+				int idx = i * this.GRID_DIMENSIONS.x + j
+						+ this.page * this.PANES_PER_PAGE;
+				if (idx >= filesList.size()) {
+					break;
+				}
+
+				File file = filesList.get(idx);
+				TextView textView;
+				if (file.isDirectory()) {
+					textView = new TextView(
+							new ContextThemeWrapper(this,
+									R.style.file_picker_pane_directory));
+					textView.setPaintFlags(
 							textView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+				} else {
+					textView = new TextView(
+							new ContextThemeWrapper(this, R.style.file_picker_pane_file));
+				}
+				textView.setText(file.getName() + (file.isDirectory() ? "/" : ""));
+				textView.setOnTouchListener(this.touchManager);
+				row.addView(textView, this.LISTING_LAYOUT_PANE);
+			}
+			this.layoutListing.addView(row, FilePickerActivity.LISTING_LAYOUT_ROW);
 		}
-		textView.setText(element);
-		this.layoutListing.addView(textView);
 	}
 }
