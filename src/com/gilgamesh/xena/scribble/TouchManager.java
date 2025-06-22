91d3c6c7a8737bd178833e1f80bb69b4f8dc5da8
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2025-06-21 22:02:50 -0600

    add back 0.1 touch ignore border

--- src/com/gilgamesh/xena/scribble/TouchManager.java
+++ src/com/gilgamesh/xena/scribble/TouchManager.java
@@ -13,7 +13,7 @@ import android.view.View;
 
 public class TouchManager implements View.OnTouchListener {
 	static private final float FLICK_MOVE_RATIO = 0.8f;
-	static private final float TOUCH_BORDER_INVALID_RATIO = 0f;
+	static private final float TOUCH_BORDER_INVALID_RATIO = 0.1f;
 	static private final int FLICK_LOWER_BOUND_MS = 80;
 	static private final int FLICK_UPPER_BOUND_MS = 260;
 	static private final int MOVE_LOWER_BOUND_MS = 260;
